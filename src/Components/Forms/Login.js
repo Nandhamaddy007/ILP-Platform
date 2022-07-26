25890ad8bb5cdb06c014ada4cf09a4608260063b
VINITH <tsvinith@gmail.com>
2022-07-26 17:20:40 +0530

    updated

--- src/Components/Forms/Login.js
+++ src/Components/Forms/Login.js
@@ -3,35 +3,35 @@ import "../Forms/forms.css";
 import { Link } from "react-router-dom";
 import image from "../../assets/logo/new_log.png";
 import axios from "axios";
- function Login() {
+
+ const Login=()=>{
  const [number,setNumber]=useState('')
  const [password,setPassword]=useState('')
-
-
-   console.log({number,password})
-
  const handleNumber =(e)=>{
+  console.log(e.target.value)
   setNumber(e.target.value)
  }
  const handlePassword =(e)=>{
   setPassword(e.target.value)
  }
  
- const handleApi=()=>{
-  console.log({number,password})
-  axios.post('',{
-    number:number,
-    password:password
-  }).then(result=>{
-console.log(result.data)
-alert ('success')
-  })
-
-  .catch(error=>{
-    alert('Enter correct mobile number and password ')
-    console.log(error)
-  })
+ const handleSubmit=(e)=>{
+e.preventDefault()
+console.log("submitted form====>")
+axios.post('',{
+number:number,
+password:password})
+.then((response)=>{
+  console.log(response.data)
+  alert('successfully')
+})
+.catch((err)=>{
+  console.log(err)
+  console.log(err.response)
+  alert('Enter the correct password')
+})
  }
+  
   return (
     <main className="class-container">
         <div className="login-body">
@@ -44,12 +44,12 @@ alert ('success')
                 <div className="login-right">
                   <div className="login-right-wrap">
                    
-    <h1 className="form__title">Login</h1>
+    <h1 className="login__title">Login</h1>
 
      
           <div className="divider"></div>
        
-        <form >
+        <form  onSubmit={handleSubmit}>
           <div className="formGroup col-sm-11 col-md-11">
             
             <input 
@@ -57,7 +57,7 @@ alert ('success')
              placeholder="Enter your Mobile Number"
               type='tel'
               value={number}
-             onChange={handleNumber}/>
+             onChange={handleNumber} required name='number'/>
             </div>
            
     
@@ -66,28 +66,28 @@ alert ('success')
              placeholder='Enter your password'
              type='password'
              value={password}
-             onChange={handlePassword}/>
+             onChange={handlePassword} required name='password'/>
           </div>
          
          
           <div className="formGroup">
             <div className="row">
               <div className="col">
-              <button type='submit'  className="submit__button" onClick={handleApi}>
+              <button type='submit' value="login" className="submit__button" >
             Login
           </button>
               </div>
                </div>
           </div>
           <p  class="paragraph">
-       <Link to="/forgot" className="new-password">Forgot Password?</Link>
+       <Link to="/resetPassword" className="new-password">Forgot Password?</Link>
     </p>
           <div className="login-or">
                     <span className="or-line"></span>
                     <span className="span-or">or</span>
                   </div>
           <div className="reg">
-          <p className="new-account">Don't have an Account?<Link to="register"  className="new-registration" > Register</Link></p>
+          <p className="new-account">Don't have an Account?<Link to="/register"  className="new-registration" > Register</Link></p>
           </div>
         </form>
         </div>
@@ -98,5 +98,5 @@ alert ('success')
         </div>
         </main>
   );
-}
+  }
 export default Login;
