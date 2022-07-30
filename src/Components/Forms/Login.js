02febcc3cf8d238b430a7b852ca4557c07da07c3
VINITH <tsvinith@gmail.com>
2022-07-30 16:14:37 +0530

    udated login and css

--- src/Components/Forms/Login.js
+++ src/Components/Forms/Login.js
@@ -1,44 +1,60 @@
 import { useState } from "react";
 import "../Forms/forms.css";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import image from "../../assets/logo/new_log.png";
-import axios from "axios";
 import Navbar from "../Navbar";
+ const Login=()=>{
+  let navigate = useNavigate();
+ const [number,setNumber]=useState('');
+ const [password,setPassword]=useState('');
+ const [numberErr,setNumberErr]=useState({});
+ const [passwordErr,setPasswordErr]=useState({});
+ 
+ 
+ const onSubmit=(e)=>{
+e.preventDefault()
+const isValid=formValidation();
+if (isValid){
+  navigate('/welcomepage')
+  setNumber('');
+  setPassword('');
+}
 
-const Login = () => {
-  const [number, setNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const handleNumber = (e) => {
-    console.log(e.target.value);
-    setNumber(e.target.value);
-  };
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
+ }
+ const formValidation=()=>{
+  var numberErr={};
+  var passwordErr={};
+  let isValid=true;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submitted form====>");
-    axios
-      .post("", {
-        number: number,
-        password: password,
-      })
-      .then((response) => {
-        console.log(response.data);
-        alert("successfully");
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log(err.response);
-        alert("Enter the correct password");
-      });
-  };
+  if (number.trim().length>10){
+    numberErr.numberShort="Invalid Number !";
+    isValid=false;
+   }
+  if (number.trim().length<10){
+    numberErr.numberShort="Invalid Number !";
+  
+    isValid=false;
+   }
+   if (password.trim().length>10){
+    passwordErr.passwordShort="Enter correct password !";
+    isValid=false;
+   }
+ 
+ if (password.trim().length<6){
+  passwordErr.passwordShort="Must be 6 character !";
+  isValid=false;
+ }
 
+ setNumberErr(numberErr);
+ setPasswordErr(passwordErr);
+ return isValid;
+
+ }
+  
   return (
     <>
       <Navbar />
-      <main className="class-container">
+    <main className="class-container">
         <div className="login-body">
           <div className="login-wrapper">
             <div className="login-container">
@@ -48,75 +64,66 @@ const Login = () => {
                 </div>
                 <div className="login-right">
                   <div className="login-right-wrap">
-                    <h1 className="login__title">Login</h1>
+                   
+    <h1 className="login__title">Login</h1>
 
-                    <div className="divider"></div>
-
-                    <form onSubmit={handleSubmit}>
-                      <div className="formGroup col-sm-11 col-md-11">
-                        <input
-                          className="form-control"
-                          placeholder="Enter your Mobile Number"
-                          type="tel"
-                          value={number}
-                          onChange={handleNumber}
-                          required
-                          name="number"
-                        />
-                      </div>
-
-                      <div className="formGroup col-sm-11 col-md-11 ">
-                        <input
-                          className="form-control"
-                          placeholder="Enter your password"
-                          type="password"
-                          value={password}
-                          onChange={handlePassword}
-                          required
-                          name="password"
-                        />
-                      </div>
-
-                      <div className="formGroup">
-                        <div className="row">
-                          <div className="col">
-                            <button
-                              type="submit"
-                              value="login"
-                              className="submit__button"
-                            >
-                              Login
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                      <p class="paragraph">
-                        <Link to="/resetPassword" className="new-password">
-                          Forgot Password?
-                        </Link>
-                      </p>
-                      <div className="login-or">
-                        <span className="or-line"></span>
-                        <span className="span-or">or</span>
-                      </div>
-                      <div className="reg">
-                        <p className="new-account">
-                          Don't have an Account?
-                          <Link to="/register" className="new-registration">
-                            {" "}
-                            Register
-                          </Link>
-                        </p>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
+     
+          <div className="divider"></div>
+       
+        <form  onSubmit={onSubmit}>
+          <div className="formGroup col-sm-11 col-md-11">
+            
+            <input 
+             className="form-control"
+             placeholder="Enter your Mobile Number"
+              type='tel'
+              value={number}
+              onChange={(e)=>{setNumber(e.target.value)}}/>
             </div>
+           {Object.keys(numberErr).map((key)=>{
+            return<div style={{color:"red",marginLeft:"10%"}}>{numberErr[key]}</div>
+           })}
+    
+          <div className="formGroup col-sm-11 col-md-11 ">
+            <input className="form-control"
+             placeholder='Enter your password'
+             type='password'
+             value={password}
+             onChange={(e)=>{setPassword(e.target.value)}}/>
           </div>
+          {Object.keys(passwordErr).map((key)=>{
+            return<div style={{color:"red",marginLeft:"10%"}}>{passwordErr[key]}</div>
+           })}
+         
+          <div className="formGroup">
+            <div className="row">
+              <div className="col">
+          <button type='submit' className="submit__button">
+            Login
+          </button>
+              </div>
+               </div>
+          </div>
+          <p  class="paragraph">
+       <Link to="/resetPassword" className="new-password">Forgot Password?</Link>
+    </p>
+          <div className="login-or">
+                    <span className="or-line"></span>
+                    <span className="span-or">or</span>
+                  </div>
+          <div className="reg">
+          <p className="new-account">Don't have an Account?<Link to="/register"  className="new-registration" > Register</Link></p>
+          </div>
+        </form>
         </div>
-      </main>
-    </>
+        </div>
+        </div>
+       </div>
+       </div>
+        </div>
+        
+        </main>
+        </>
   );
-};
+  }
 export default Login;
