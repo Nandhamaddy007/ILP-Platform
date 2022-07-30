8e3c8fb82aada66f2e727e998cdb90a2da0abef2
Nandhagopal <nandhamaddy007@gmail.com>
2022-07-30 19:26:27 +0530

    mobile view added filter screen changes added

--- src/Components/Forms/EditStudent.js
+++ src/Components/Forms/EditStudent.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 export default function EditStudent(props){
-  const [obj,setObj]= useState({pTask:""})
+  const [obj,setObj]= useState({pTask:"",sTask:""})
     useEffect(()=>{
 setObj({...props.data})
     },[props])
@@ -33,22 +33,44 @@ data-bs-backdrop="static" data-bs-keyboard="false"
         </div>
         <div className="row mt-3">
             <div className="col"><label>Primary Task:</label></div>
-            <div className="col">  <select className="form-control" name="type" id="type" value={obj.pTask}>
+            <div className="col">  <select 
+            className="form-control" 
+            name="type" 
+            id="type" 
+            onChange={(e)=>{
+              setObj({
+                ...obj,
+                pTask:e.target.value
+              })
+            }}
+            value={obj.pTask}>
                 <option></option>
-                    <option>doctor</option>
-                    <option>engineer</option>
-                    <option>lawyer</option>
-                    <option>scientist</option>
-                    <option>police</option>
-                    <option>chef</option>
-                    <option>farmer</option>
+                <option>Doctor</option>
+                    <option>Engineer</option>
+                    <option>Lawyer</option>
+                    <option>Scientist</option>
+                    <option>Police</option>
+                    <option>Chef</option>
+                    <option>Farmer</option>
+                    <option>Pilot</option>
+                    <option>Politician</option>
+                    <option>Business</option>
                 </select>
             {/* <div className="col"><label>{props.data.pTask}</label></div> */}
             </div>     
         </div>
         <div className="row mt-3">
             <div className="col"><label>Secondary Task:</label></div>
-            {/* <div className="col"> <select className="form-control" name="type"  id="type" value={props.data.sTask}>
+            <div className="col"> <select className="form-control" 
+            name="type"  
+            id="type" 
+            onChange={(e)=>{
+              setObj({
+                ...obj,
+                sTask:e.target.value
+              })
+            }}
+            value={obj.sTask}>
                 <option></option>
                     <option>Doctor</option>
                     <option>Engineer</option>
@@ -57,9 +79,12 @@ data-bs-backdrop="static" data-bs-keyboard="false"
                     <option>Police</option>
                     <option>Chef</option>
                     <option>Farmer</option>
+                    <option>Pilot</option>
+                    <option>Politician</option>
+                    <option>Business</option>
                 </select>            
-            </div> */}
-            <div className="col"><label>{props.data.sTask}</label></div>
+            </div>
+            {/* <div className="col"><label>{props.data.sTask}</label></div> */}
         </div>
         {props.data.pTask?.length>1 && <div className="row mt-3">
             <div className="col"><label htmlFor="">Comments: </label></div>
@@ -71,7 +96,9 @@ data-bs-backdrop="static" data-bs-keyboard="false"
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success">Save changes</button>
+        <button type="button" class="btn btn-success"data-bs-dismiss="modal"
+        onClick={()=>{alert("Changes saved successfully")}}
+        >Save changes</button>
       </div>
     </div>
   </div>
